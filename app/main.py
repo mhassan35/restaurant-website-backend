fcbedf615c0c9df34aa9a5da6a16e60a32eea06d
mhassan35 <mhassan909234@gmail.com>
2025-12-14 21:32:17 +0500

    Refactor main.py to streamline FastAPI setup and update CORS origins
    - Removed unnecessary comments and code for a cleaner FastAPI initialization.
    - Updated CORS origins for better clarity and organization.
    - Simplified the inclusion of the menu router in the application setup.

--- app/main.py
+++ app/main.py
@@ -1,25 +1,19 @@
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from app.routers import menu 
+from app.routers import menu
 
-app = FastAPI(title="Restaurant Backend")
-
+app = FastAPI()
 origins = [
-    "http://localhost:3000",
+    "http://localhost:3000",  # React dev server
     "https://restaurant-website-delta-neon.vercel.app",
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"], 
+    allow_headers=["*"],
 )
-
-# Mount the static folder to serve images
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-# Include the menu router
-app.include_router(menu.router, prefix="/menu", tags=["menu"])
+app.include_router(menu.router)
